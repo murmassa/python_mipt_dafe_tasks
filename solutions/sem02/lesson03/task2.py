ad109bbd430da33d7bee59e9856e48df7a549ec2
murmassa <egorgalkin909@gmail.com>
2026-03-08 23:26:07 +0300

    dz lesson03

--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -9,11 +9,33 @@ def convert_from_sphere(
     distances: np.ndarray,
     azimuth: np.ndarray,
     inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]: 
+    
+    if not (distances.shape == azimuth.shape == inclination.shape):
+        raise ShapeMismatchError
+    
+    abscissa = distances * np.sin(inclination) * np.cos(azimuth)
+    ordinates = distances * np.sin(inclination) * np.sin(azimuth)
+    applicates = distances * np.cos(inclination)
+    
+    return abscissa, ordinates, applicates
 
 
 def convert_to_sphere(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]: 
+    
+    if not (abscissa.shape == ordinates.shape == applicates.shape):
+        raise ShapeMismatchError
+    
+    r = np.sqrt(abscissa**2 + ordinates**2 + applicates**2)
+    azimuth = np.arctan2(ordinates, abscissa)
+    
+    inclination = np.zeros_like(r)
+    
+    mask = r != 0
+    inclination[mask] = np.arccos(applicates[mask] / r[mask])
+    
+    return r, azimuth, inclination
--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,19 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]: 
+    
+    if len(ordinates) < 3:
+        raise ValueError
+   
+    left_compare = ordinates[1:-1] > ordinates[:-2]
+    right_compare = ordinates[1:-1] > ordinates[2:] 
+    
+    max = left_compare & right_compare
+    
+    min = (~left_compare) & (~right_compare)
+    
+    max_indices = np.where(max)[0] + 1
+    min_indices = np.where(min)[0] + 1
+    
+    return min_indices, max_indices
